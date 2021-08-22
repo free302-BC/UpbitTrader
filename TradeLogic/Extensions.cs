21aabe8fa4785f51ce28ae7257213e561eddae7d
DrBAE <amadeus.bae@gmail.com>
2021-08-23 02:45:22 +0900

    remove some util classes & methods

--- TradeLogic/Extensions.cs
+++ TradeLogic/Extensions.cs
@@ -23,65 +23,6 @@ namespace Universe.Coin.TradeLogic
         public static string Print(this IEnumerable<IViewModel> models, int offset, int count)
             => IViewModel.Print(models, offset, count);
 
-
-        #region ---- move to Universe.Utility ----
-
-        /// <summary>
-        /// 멤버의 getter 'Func'과 setter 'Action'을 리턴
-        /// </summary>
-        /// <typeparam name="P">멤버의 데이터 타입</typeparam>
-        /// <param name="selector">instance를 포함한 member access func</param>
-        /// <returns></returns>
-        public static (Func<P> getter, Action<P> setter)
-            ToDelegate<P>(this Expression<Func<P>> selector)//P=Property
-        {
-            //selector.Body := member access expression
-            //var v0 = selector.Compile()();//현재값
-            var paramMember = Expression.Parameter(selector.Body.Type);//== P
-            var assignExp = Expression.Lambda<Action<P>>(Expression.Assign(selector.Body, paramMember), paramMember);
-            var setter = assignExp.Compile();
-
-            return (selector.Compile(), setter);
-        }
-
-        public static (Func<I, P> getter, Action<I, P> setter)
-            ToDelegate<I, P>(this Expression<Func<I, P>> selector)//I=Instance,P=Property
-        {
-            var paramInstance = selector.Parameters[0];//== I
-            var paramMember = Expression.Parameter(selector.Body.Type);//parameter expression == P
-            var assignExp = Expression.Lambda<Action<I, P>>(
-                Expression.Assign(selector.Body, paramMember), paramInstance, paramMember);
-            var setter = assignExp.Compile();
-
-            return (selector.Compile(), setter);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="I"></typeparam>
-        /// <typeparam name="P"></typeparam>
-        /// <param name="instance"></param>
-        /// <param name="selector"></param>
-        /// <returns></returns>
-        public static (P currentValue, Action<P> setter)
-            ToDelegate<I, P>(this I instance, Expression<Func<I, P>> selector)//I=Instance,P=Property
-        {
-            var v0 = selector.Compile()(instance);
-
-            var paramInstance = selector.Parameters[0];//== I
-            var paramMember = Expression.Parameter(selector.Body.Type);//parameter expression == P
-            var assignExp = Expression.Lambda<Action<I, P>>(
-                Expression.Assign(selector.Body, paramMember), paramInstance, paramMember);
-            var setter = assignExp.Compile();
-
-            Action<P> setterWithoutInstance = p => setter(instance, p);
-
-            return (v0, setterWithoutInstance);
-        } 
-        #endregion
-
-
         public static void LogWebException(this ILogger logger, HttpRequestException ex)
         {
             var nl = Environment.NewLine;
